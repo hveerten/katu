f7a086ab6179387f3fbc3d7e2f48ac0136a5ff26
Bruno Jiménez <brunojimen@gmail.com>
2019-01-21 15:58:20 +0000

    Pion Decay: Add a missing gamma_pion

--- src/pion_decay.c
+++ src/pion_decay.c
@@ -91,8 +91,8 @@ void charged_pion_decay(state_t *st)
 
             if(x < 1 - MASSES_RATIO && x < 1)
             {
-                muon_neutrino_production     += 2 * n_positive_pion / (g_pion * (1 - MASSES_RATIO));
-                muon_antineutrino_production += 2 * n_negative_pion / (g_pion * (1 - MASSES_RATIO));
+                muon_neutrino_production     += 2 * n_positive_pion / (g_pion * (1 - MASSES_RATIO)) / g_pion;
+                muon_antineutrino_production += 2 * n_negative_pion / (g_pion * (1 - MASSES_RATIO)) / g_pion;
             }
         }
 
@@ -196,8 +196,8 @@ void calculate_pion_decay_LUT_muon_functions(state_t *st)
             double g_pion = st->positive_pions.energy[j];
             double x = g_muon * MUON_MASS / (g_pion * PION_MASS);
 
-            st->pion_decay_LUT_positive_right[index_base + j] = f_positive_right(x) / g_pion;
-            st->pion_decay_LUT_positive_left [index_base + j] = f_positive_left(x)  / g_pion;
+            st->pion_decay_LUT_positive_right[index_base + j] = f_positive_right(x) / (g_pion * g_pion);
+            st->pion_decay_LUT_positive_left [index_base + j] = f_positive_left(x)  / (g_pion * g_pion);
         }
     }
 }
